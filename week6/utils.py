52a62818b03764292c4a58800cac76f482142110
shekit <as7790@nyu.edu>
2017-04-07 04:16:34 -0400

    week 6 utils for probability sampling

---
+++ week6/utils.py
@@ -0,0 +1,8 @@
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
